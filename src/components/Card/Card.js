2b07484058e0d41605d1aec3cd6f63ecd15ee739
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-01-03 11:26:41 +0900

    ポケモンのデータを一覧表示

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Card = ({ pokemon }) => {
     return (
-        <div>Card</div>
+        <div className='card'>
+            <div className='cardImg'>
+                <img src={pokemon.sprites.front_default} alt="" />
+            </div>
+            <h3 className='cardName'>{pokemon.name}</h3>
+            <div className="cardTypes">
+                <div>Type</div>
+                {pokemon.types.map((type) => {
+                    return <div>
+                        <span className='typeName'>{type.type.name}</span>
+                    </div>
+                })}
+            </div>
+            <div className="cardInfo">
+                <div className="cardData">
+                    <p className='title'>Weight: {pokemon.weight}</p>
+                </div>
+                <div className="cardData">
+                    <p className='title'>Height: {pokemon.height}</p>
+                </div>
+                <div className="cardData">
+                    <p className='title'>Ability: {pokemon.abilities[0].ability.name}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
