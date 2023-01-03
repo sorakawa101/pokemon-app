65de27beab94e8d501a564f8d50b0e28e2b2231d
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-01-03 10:30:57 +0900

    add: loadingの状態を設定

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import {getAllPokemon} from './utils/pokemon'
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      {loading ? (
+        <h1>loading now...</h1>
+      ) : (
+        <h1>Successfully fetched data!!</h1>
+      )}
+    </div>
   );
 }
 
