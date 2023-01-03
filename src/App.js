451b3af3fba1a5a4efe466bde80e540cfce94509
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-01-03 10:20:14 +0900

    add: データを取得してコンソールに表示

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
+import {getAllPokemon} from './utils/pokemon'
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
   return (
     <div className="App"></div>
   );
